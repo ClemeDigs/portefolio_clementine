e88e77d9b249e70e63497e7f7af610d34d8f7461
Clementine Digout <clementine.digout@yahoo.fr>
2024-08-07 20:47:59 -0400

    change main theme and debug

--- src/assets/js/change_project.js
+++ src/assets/js/change_project.js
@@ -1,15 +1,31 @@
 const projectsDetails = document.querySelectorAll('.card-detail');
 const btnsNext = document.querySelectorAll('.btn-next');
 const btnsPrevious = document.querySelectorAll('.btn-previous');
+const modalsContents = document.querySelectorAll('.project-details-container'); 
+
+let currentIndex = 0;
 
 btnsNext.forEach(btnNext => {
+    
     btnNext.addEventListener('click', () => {
-        changeCard();
+        console.log(projectsDetails.length);
+        currentIndex = (currentIndex + 1);
+        console.log(currentIndex);
+        updateModalContent(currentIndex);
     });
-})
+});
 
-/* function changeCard() {
-    projectsDetails.forEach(projectDetail, index => {
-        const projectDetail = projectsDetails[index];
+
+btnsPrevious.forEach(btnPrevious => {
+    btnPrevious.addEventListener('click', () => {
+        currentIndex = (currentIndex - 1);
+        updateModalContent(currentIndex);
+    });
+});
+
+
+function updateModalContent() {
+    projectsDetails.forEach(projectDetail => {
+        projectDetail.innerHtml = projectDetail[currentIndex];
     })
-} */
+}
